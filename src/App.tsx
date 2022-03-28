83e425ede6abf9bd376a4436979fac68216fa97d
MAKS <5059585@mail.ru>
2022-03-28 21:27:23 +0300

    js 2 28.03.22

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,65 @@
 import React from 'react';
 import './App.css';
-
-let students=[
+let students = [
     {
-        name: "Alex",
+        name: 'Alex',
         age: 21,
         isMarried: true,
         scores: 89,
     },
     {
-        name: "Nick",
+        name: 'Nick',
         age: 20,
         isMarried: false,
         scores: 120,
     },
     {
-        name: "bob",
+        name: 'Bob',
         age: 22,
         isMarried: true,
         scores: 85,
-        fn: () => {
-        }
     },
 ]
 
-students.map()
+console.log(students.map(s => s.name))
+console.log(
+    myMap(students, (s:any) => s.name))
+function myMap(array:any, callback:any) {
+    const newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray[i] = callback(array[i])
+    }
+    return newArray
+}
+console.log(myMap(students, (s:any) => s.name))
+
+console.log(students.filter(s=>s.scores>=100))
+console.log(myFilter(students,(s:any)=>s.scores>=100))
+function myFilter(array:any,callback:any){
+    const newArray=[]
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i])===true){
+            newArray.push(array[i])        }
+    }
+    return newArray
+}
+
+console.log(students.find(s=>s.name==="Bob"))
+console.log(myFind(students, (s:any)=>s.name==="Bob"))
+function myFind(array:any,callback:any) {
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i])) {
+            return array[i];
+        }
+    }
+}
+
 
 
 function App() {
     return (
         <>
-    </>
+        </>
     );
 }
 
